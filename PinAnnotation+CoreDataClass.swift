2304efd8a3a69e59692b6bb15912217b04265844
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2018-02-11 03:40:02 -0800

    feat: Added the CoreData model and CoreDataStack

---
+++ PinAnnotation+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PinAnnotation+CoreDataClass.swift
+//  VirtualTourist_8
+//
+//  Created by Michelle Grover on 2/11/18.
+//  Copyright © 2018 Norbert Grover. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class PinAnnotation: NSManagedObject {
+
+}
