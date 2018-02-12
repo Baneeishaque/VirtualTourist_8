f1d4d2e4d8300625b2477e1d6fe05cb66bd18ad4
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2018-02-11 19:05:14 -0800

    feat: A UIButton and a collectionView has been added to the MapDetailViewController

--- VirtualTourist_8/MapDetailViewController.swift
+++ VirtualTourist_8/MapDetailViewController.swift
@@ -10,17 +10,49 @@ import UIKit
 import CoreData
 import MapKit
 
-class MapDetailViewController: UIViewController {
+class MapDetailViewController: UIViewController, MKMapViewDelegate {
     
-    var locationAnnotation:MKAnnotation?
+    var locationAnnotation:MKAnnotation!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Test annotation:lat:\(String(describing: locationAnnotation?.coordinate.latitude)), long:\(String(describing: locationAnnotation?.coordinate.longitude))")
+        
+        // Mark: Set the mapView delegate
+        mapView.delegate = self
+        setSpanAndRegion()
+        setAnnotation()
+       
     }
 
     
 
 
+}
+
+
+
+// Mark: This is the functionality for setting a pin on the mapView
+extension MapDetailViewController {
+    
+    // Mark: Create a pin and pass the coordinates
+    func setAnnotation() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationAnnotation.coordinate
+        annotation.title = ""
+        annotation.subtitle = ""
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    // Mark: Mapkit also requires that you add a span and region
+    func setSpanAndRegion() {
+        let latDelta:CLLocationDegrees = 1.00
+        let longDelta:CLLocationDegrees = 1.00
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let region = MKCoordinateRegion(center: locationAnnotation.coordinate, span: span)
+        self.mapView.setRegion(region, animated: true)
+    }
 }
 
